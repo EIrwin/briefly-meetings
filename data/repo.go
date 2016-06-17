698cdc8ed4ddc7eee52edf773a8ba979a0859bb4
= <Eric.Irwin1124@gmail.com>
2016-06-16 22:27:16 -0700

    Added service functionality for several calls

--- data/repo.go
+++ data/repo.go
@@ -1,20 +1,107 @@
 package data
 
 import (
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"log"
 	"time"
 )
 
 func CreateMeeting(meeting *Meeting) error {
+	endpoint, db := getConnectionInfo()
+
+	session, err := mgo.Dial(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	//Collection Meetings
+	c := session.DB(db).C("meetings")
+
+	// Index
+	index := mgo.Index{
+		Key:        []string{"id"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	if err := c.EnsureIndex(index); err != nil {
+		log.Fatal(err)
+	}
+
+	//Insert
+	meeting.Id = bson.NewObjectId()
+	err = c.Insert(meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
 
-func GetMeeting(id string) error {
+func GetMeeting(id string) (Meeting, error) {
+	endpoint, db := getConnectionInfo()
+
+	session, err := mgo.Dial(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	c := session.DB(db).C("meetings")
+	var meeting Meeting
+	err = c.Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return meeting, nil
+}
+
+func UpdateMeeting(meeting *Meeting) error {
+	endpoint, db := getConnectionInfo()
+
+	session, err := mgo.Dial(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	//Collection Meetings
+	c := session.DB(db).C("meetings")
+
+	// Index
+	index := mgo.Index{
+		Key:        []string{"id"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	if err := c.EnsureIndex(index); err != nil {
+		log.Fatal(err)
+	}
+
+	// Update
+	err = c.Update(bson.M{"_id": bson.ObjectIdHex(meeting.Id.Hex())}, meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
 
-func UpdateMeeting(Meeting *Meeting) error {
-	return nil
+func getConnectionInfo() (string, string) {
+	endpoint := "mongodb://dev:briefly123!@ds015584.mlab.com:15584/briefly"
+	db := "briefly"
+	return endpoint, db
 }
 
 type Meeting struct {
--- services/meetings.go
+++ services/meetings.go
@@ -2,32 +2,154 @@ package services
 
 import (
 	"github.com/eirwin/briefly-meetings/data"
+	"log"
+	"time"
 )
 
-func CreateMeeting() (data.Meeting, error) {
-	return nil
+func Create(req CreateMeeting) (data.Meeting, error) {
+	var meeting data.Meeting
+	err := data.CreateMeeting(&meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return meeting, nil
 }
 
-func GetMeeting(id string) (data.Meeting, error) {
-	return nil
+func Get(req GetMeeting) (data.Meeting, error) {
+	meeting, err := data.GetMeeting(req.MeetingId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if meeting.Active {
+
+		meeting, err = updateTotal(meeting)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return meeting, nil
 }
 
-func StartMeeting(id string) (data.Meeting, error) {
-	return nil
+func Start(req StartMeeting) (data.Meeting, error) {
+	meeting, err := data.GetMeeting(req.MeetingId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	meeting.Start = time.Now().UTC()
+	meeting.Active = true
+	err = data.UpdateMeeting(&meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return meeting, nil
 }
 
-func EndMeeting(id string) (data.Meeting, error) {
-	return nil
+func End(req EndMeeting) (data.Meeting, error) {
+	meeting, err := data.GetMeeting(req.MeetingId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	meeting.End = time.Now().UTC()
+	meeting.Active = false
+	err = data.UpdateMeeting(&meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return meeting, nil
 }
 
-func JoinMeeting(id string) (data.Meeting, error) {
-	return nil
+func Join(req JoinMeeting) (data.Meeting, error) {
+
+	//get user using user id
+
+	meeting, err := data.GetMeeting(req.MeetingId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//add user to meeting
+	//meeting.Users = append(meeting.Users)
+
+	err = data.UpdateMeeting(&meeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return meeting, nil
 }
 
-func LeaveMeeting(id string) (data.Meeting, error) {
-	return nil
+func Leave(req LeaveMeeting) (data.Meeting, error) {
+
+	//get user using user id
+
+	//	var user = await _userService.GetUserById(new GetUserById() { Id = leaveMeeting.UserId });
+	//
+	//var meeting = _meetingRepository.AsQueryable().FirstOrDefault(p => p.Id == leaveMeeting.MeetingId);
+	//
+	//var attendees = new List<User>();
+	//foreach (User attendee in meeting.Attendees)
+	//{
+	//if (attendee.Id != user.Id)
+	//attendees.Add(new User()
+	//{
+	//Id = attendee.Id, HourlyRate = attendee.HourlyRate, Salary = attendee.Salary
+	//});
+	//}
+	//meeting.Attendees = attendees;
+	//meeting = _meetingRepository.Save(meeting);
+	return data.Meeting{}, nil
 }
 
-func UpdateTotal() (data.Meeting, error) {
-	return nil
+func updateTotal(m data.Meeting) (data.Meeting, error) {
+	total := m.Total
+	minutesPassed := time.Now().UTC().Sub(m.Start).Minutes()
+	hourlyPercent := minutesPassed / 60
+
+	for _, val := range m.Users {
+		hourlyRate, err := getHourlyRate(val)
+		if err != nil {
+			log.Fatal(err)
+		}
+		total += hourlyRate * float32(hourlyPercent)
+		m.Total = total
+		data.UpdateMeeting(&m)
+	}
+
+	return m, nil
+}
+
+func getHourlyRate(u data.MeetingUser) (float32, error) {
+	const hours = 2080 //work hours per year
+	if u.Salary > 0 {
+		return (u.Salary / hours), nil
+	}
+	return u.HourlyRate, nil
+}
+
+type CreateMeeting struct {
+}
+
+type GetMeeting struct {
+	MeetingId string
+}
+
+type StartMeeting struct {
+	MeetingId string
+}
+
+type EndMeeting struct {
+	MeetingId string
+}
+
+type JoinMeeting struct {
+	MeetingId string
+	UserId    string
+}
+
+type LeaveMeeting struct {
+	MeetingId string
+	UserId    string
 }
